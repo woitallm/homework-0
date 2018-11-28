295deb638286308e1421f96ea7d7213b8b0dd9db
woitallm <woitallm@enbridge.com>
2018-11-28 09:46:07 -0600

    updated unix commands

--- code.R
+++ code.R
@@ -1,3 +1,28 @@
 library(tidyverse)
 
 #look up "gitignore"
+#might need to learn more about UNIX, perhaps take a course in it.
+
+#unix commmands:
+  #pwd - present working directory
+  #mkdir - make directory; adds to the present directory
+  #rmdir - remove directory; dir must be empty
+  #rm - removes non-empty dir; is permenant
+  #cd - changes directory; starts from present directory; cd alone will bring you back to home dir
+  #ls - LS lists the directory content
+    #note that pressing tab will autocomplete a dir if that is the only start, e.g. D tab will bring up Desktop
+  #cd .. - parent directory; e.g. cd .. moves to parent dir
+  #cd . - current directory; wont change directories
+  #cd ~ - returns to home dir; e.g. cd ~ moves to home dir
+  #cd - - returns to previous dir
+  #mv - is a move command; note that there is no confirmation
+      #can also rename e.g. cd ../file.text ../other.text
+  #cp - similar to move, but copies instead
+  #less - look at the file; type q to exit less
+
+
+
+
+
+
+
